f8a6b8821223d9cc7d8e2c6f1b6d897209afb299
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-01-15 12:42:00 +0300

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
---  lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+-- Lists all bands with Glam rock AS their main style, ranked by their longevity
+
+SELECT  band_name,(ifnull(split,2020) - ifnull(formed,0)) lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+ORDER BY lifespan DESC 
